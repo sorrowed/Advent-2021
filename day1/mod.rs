b13de8146abc55e791f84bea5c6ed9289e63110a
Tom Bulsink <tom.bulsink@inspiro.nl>
2021-12-02 17:57:35 +0100

    Day 1 REFACTOR vector also has a windows

--- day1/mod.rs
+++ day1/mod.rs
@@ -1,35 +1,29 @@
 use crate::common;
-use itertools::Itertools;
 
-fn find_descending_1(a: &Vec<i64>) -> i64 {
-
-    let mut deeper = 0;
-
-    for i in 1..a.len() {
-        if a[i] > a[i-1] {
-            deeper+=1;
-        }
-    }
-
-    deeper
+fn find_descending_1(a: &Vec<i64>) -> usize {
+    a.windows(2).filter(|w| w[0] < w[1]).count()
 }
 
-fn find_descending_2(a: &Vec<i64>) -> i64 {
-
-    let mut windows = vec!();
-    for (a, b,c) in a.iter().tuple_windows() {
-        windows.push(a+b+c);
-    }
-
-    find_descending_1(&windows)
+fn find_descending_2(a: &Vec<i64>) -> usize {
+    find_descending_1(&a.windows(3).map(|w| w.iter().sum()).collect::<Vec<i64>>())
 }
 
 pub fn part1() {
-    let m = find_descending_1(&common::import("day1/input.txt").iter().map(|a|a.parse::<i64>().unwrap()).collect());
+    let m = find_descending_1(
+        &common::import("day1/input.txt")
+            .iter()
+            .map(|a| a.parse::<i64>().unwrap())
+            .collect(),
+    );
     println!("Descending : {}", m);
 }
 
 pub fn part2() {
-    let m = find_descending_2(&common::import("day1/input.txt").iter().map(|a|a.parse::<i64>().unwrap()).collect());
+    let m = find_descending_2(
+        &common::import("day1/input.txt")
+            .iter()
+            .map(|a| a.parse::<i64>().unwrap())
+            .collect(),
+    );
     println!("Descending : {}", m);
 }
